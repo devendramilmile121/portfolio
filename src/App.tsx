b083f42c1654aa58f2f9da41e7ccc8b34066b772
Devendra Milmile <d.h.milmile1@gmail.com>
2025-07-28 23:35:34 +0530

    Fixed root level routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,12 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const isProd = import.meta.env.MODE === "production";
-const basename = isProd ? "/portfolio" : undefined;
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={basename}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
