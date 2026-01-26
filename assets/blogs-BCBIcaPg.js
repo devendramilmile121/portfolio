f73296c6b1c9559311be40e4c838bbbc3d0f91b9
devendramilmile121 <devendramilmile121@users.noreply.github.com>
2026-01-26 11:47:07 +0000

    deploy: 50620678cc4b73adec6c953c29e857814d3fd956

--- assets/blogs-BCBIcaPg.js
+++ assets/blogs-BCBIcaPg.js
@@ -1080,7 +1080,7 @@ Always remember to:
 Happy coding! 🚀
 `,u=`---
 title: "Angular Deferrable Views: Optimizing Bundle Size with @defer Blocks"
-date: "2026-01-09"
+date: "2026-01-26"
 summary: "Master Angular's @defer blocks to intelligently defer non-critical code, improve Core Web Vitals, and create lightning-fast applications with granular control over lazy loading."
 tags: ["Angular", "Performance", "Optimization", "Web Vitals", "TypeScript", "Web Development"]
 ---
